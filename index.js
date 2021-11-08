205ef1e754b5c0dfe73eaa3f3813e4f1a5d0d073
Seungwoo Paek <swpaek77@gmail.com>
2021-11-08 15:07:39 +0900

    IP 가져오도록 수정

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require("axios");
   try {
     const { data } = await axios.get("https://www.sunssc.com/ip/");
     console.log(data);
+    console.log(data[0].IP);
   } catch (err) {
     console.log(err);
   }
